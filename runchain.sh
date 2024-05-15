12d6ec86d6efc7e1ceb84617660f59f334bdf1e5
weegerry <163557824+weegerry@users.noreply.github.com>
2024-05-15 19:33:32 +0100

    Update runchain.sh

--- runchain.sh
+++ runchain.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
-echo "Setup /root/.multichain/multichain.conf"
-mkdir -p /root/.multichain/
-cat << EOF > /root/.multichain/multichain.conf
+
+# Create the MultiChain configuration directory if it doesn't exist
+if [ ! -d /root/.multichain/ ]; then
+    mkdir -p /root/.multichain/
+fi
+
+# Create the multichain.conf file only if it doesn't exist
+if [ ! -f /root/.multichain/multichain.conf ]; then
+    echo "Setup /root/.multichain/multichain.conf"
+    cat << EOF > /root/.multichain/multichain.conf
 rpcallowip=$RPC_ALLOW_IP
 rpcport=$RPC_PORT
 EOF
+else
+    echo "/root/.multichain/multichain.conf already exists. Skipping creation."
+fi
 
 echo "Start the chain"
 multichaind -txindex -printtoconsole -shrinkdebugfilesize $CHAINNAME@$MASTER_NODE:$NETWORK_PORT
@@ -13,4 +23,4 @@ multichaind -txindex -printtoconsole -shrinkdebugfilesize $CHAINNAME@$MASTER_NOD
 sleep 30
 
 # Run the multichain-cli command and output to the terminal
-/bin/bash -c "multichain-cli base_chain_v2 getaddresses"
+/bin/bash -c "multichain-cli $CHAINNAME getaddresses"
